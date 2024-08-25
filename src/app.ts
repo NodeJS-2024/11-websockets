ddc55b26d3c83ab1d930cdae14f2fcca0f9d7e45
Luis Angel <angelfgdeveloper@gmail.com>
2024-08-25 15:16:18 -0600

    Añadiendo websocket

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,16 @@
+import { WebSocketServer } from 'ws';
 
-console.log('Hola Mundo');
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+
+  console.log('Client connected');
+
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('Hola desde el servidor');
+});
