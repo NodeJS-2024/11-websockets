76d42485b381c8a7d5d8da19ec8478eec727864b
Luis Angel <angelfgdeveloper@gmail.com>
2024-08-25 22:20:17 -0600

    Integrando payload y mensaje

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,36 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 3000 });
+const webSocketServer = new WebSocketServer({ port: 3000 });
 
-wss.on('connection', function connection(ws) {
+webSocketServer.on('connection', function connection(ws) {
 
   console.log('Client connected');
 
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    // console.log('received: %s', data);
+    console.log('Desde el cliente', data);
+
+    const payload = {
+      type: 'custom-message',
+      payload: data.toString()
+    }
+
+    // ws.send(data.toString().toUpperCase());
+    ws.send(JSON.stringify(payload));
   });
 
-  ws.send('Hola desde el servidor');
+  //ws.send('Hola desde el servidor!');
+
+  // setInterval(() => {
+  //   ws.send('Hola de nuevo');
+  // }, 2000);
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+
 });
+
+console.log('Server running on port http://localhost:3000');
